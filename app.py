55bfc7fbc6d54045852aba8e7cc818e2aa55bb29
harmain Nasim <82891376+HarmainNasim@users.noreply.github.com>
2023-10-29 16:52:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def welcome():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
